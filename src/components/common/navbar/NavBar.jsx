4807e12d39019714a642ca26620057d821360316
HelithaSri <helitha.pravin@gmail.com>
2022-08-27 14:38:16 +0530

    right side style done

--- src/components/common/navbar/NavBar.jsx
+++ src/components/common/navbar/NavBar.jsx
@@ -5,14 +5,27 @@ import navLogo from '../../../assets/img/logo/logo-YTS.png'
 function NavBar() {
   return (
     <header className={classes.navBar}>
-      <div className={classes.logoDiv}>
+      <div className={classes.leftDiv}>
         <img src={navLogo} alt="" />
         <span className={classes.navText}>
           HD movies at the smallest file size.
         </span>
       </div>
-      <div>
-        <h1>Hellod</h1>
+      <div className={classes.rightDiv}>
+          <input
+            className={classes.quick_search_input}
+            name="query"
+            autocomplete="off"
+            type="search"
+            value="Quick search"
+          />
+        <ul className={classes.navLinks}>
+          <li>Home</li>
+          <li>4K</li>
+          <li>Trending</li>
+          <li>Browse Movies</li>
+          <li><a href="">Login</a>  |  <a href="">Register</a></li>
+        </ul>
       </div>
     </header>
   )
