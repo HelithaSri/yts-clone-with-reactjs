09d6ba71e0123e32e6684684c53a4104e918f6a5
HelithaSri <helitha.pravin@gmail.com>
2022-08-27 23:24:13 +0530

    create home page

---
+++ src/pages/Hero Section/index.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import Card from './../../components/common/movie card/Card'
+
+function HeroSection() {
+  return (
+    <section style={{ backgroundColor: '#171717' }}>
+      <div>HeroSection</div>
+
+      {/* <Card /> */}
+      <div style={{ display: 'flex', gap: '10px' }}>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+      <div style={{ display: 'flex', gap: '10px' }}>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+      <div style={{ display: 'flex', gap: '10px' }}>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </section>
+  )
+}
+
+export default HeroSection
