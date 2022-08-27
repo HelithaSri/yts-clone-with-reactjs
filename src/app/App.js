a98e071685198886de0d656d006990732df371ce
HelithaSri <helitha.pravin@gmail.com>
2022-08-27 23:25:31 +0530

    add hero page

--- src/app/App.js
+++ src/app/App.js
@@ -1,11 +1,12 @@
 import NavBar from './../components/common/navbar/NavBar';
+import HeroSection from './../pages/Hero Section/index';
 
 function App() {
   return (
     <>
         <NavBar/>
         <main>
-          das
+          <HeroSection/>
         </main>
     </>
   );
