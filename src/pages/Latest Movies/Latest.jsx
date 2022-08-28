aaf936f3049f76d3365f4cc693a8c12fb1e18ece
HelithaSri <helitha.pravin@gmail.com>
2022-08-28 15:10:05 +0530

    latest section done

--- src/pages/Latest Movies/Latest.jsx
+++ src/pages/Latest Movies/Latest.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import styles from './Latest.module.css'
+import Card from './../../components/movie card/Card';
 
 function LatestMovies() {
   return (
-    <section className={styles.section}></section>
+    <section className={styles.section}>
+      <div className={styles.container}>
+        <div className={styles.latestTitle}>
+          <h3>Latest YIFY Movies Torrents</h3>
+          <a href="#"><p>Browse All</p></a>
+        </div>
+        <div className={styles.latest_row}>
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+        </div>
+        <div className={styles.latest_row}>
+          <Card/>
+          <Card/>
+          <Card/>
+          <Card/>
+        </div>
+      </div>
+    </section>
   )
 }
 
